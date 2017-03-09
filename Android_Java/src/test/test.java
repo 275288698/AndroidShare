84ecaf49813dad9051db39aa8057eb7f12ebef7b
YIQIN295 <yiqin.androiddev@gmail.com>
2017-03-09 19:26:42 +0800

    some stastic data

---
+++ Android_Java/src/test/test.java
@@ -0,0 +1,15 @@
+package test;
+
+public class test {
+
+	public static void main(String[] args){
+		for (int i = 0; i < args.length; i++) {
+			System.out.println("---  "+args[i]);
+		}
+		
+		int a = 1<<3;
+		System.out.println("---  "+a);
+		
+	}
+	
+}
