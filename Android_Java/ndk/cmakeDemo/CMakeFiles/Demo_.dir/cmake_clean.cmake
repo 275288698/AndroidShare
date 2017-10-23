0b56514b69ba1067d3b4b7c441d1522f7d8f4c5d
YIQIN295 <yiqin.androiddev@gmail.com>
2017-10-23 15:26:36 +0800

    cmake

---
+++ Android_Java/ndk/cmakeDemo/CMakeFiles/Demo_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Demo_.dir/main.cc.o"
+  "Demo_.pdb"
+  "Demo_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Demo_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Android_Java/ndk/cmakeDemo/math/CMakeFiles/MFun.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MFun.dir/MFun.cc.o"
+  "libMFun.pdb"
+  "libMFun.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MFun.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
